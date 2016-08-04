d211aa1967db520ad529bca892c57c30f9836083
Larry Garfield <larry@garfieldtech.com>
2016-08-04 15:39:58 -0500

    Include default constructors for the generic classes.

--- src/GenericLinkCollection.php
+++ src/GenericLinkCollection.php
@@ -8,4 +8,20 @@ use Psr\Link\LinkInterface;
 class GenericLinkCollection implements EvolvableLinkCollectionInterface
 {
     use EvolvableLinkCollectionTrait;
+
+    /**
+     * Constructs a new link collection.
+     *
+     * @param LinkInterface[] $links
+     *   Optionally, specify an initial set of links for this collection.
+     *   Note that the keys of the array will be ignored.
+     */
+    public function __construct(array $links = [])
+    {
+        // This block will throw a type error if any item isn't a LinkInterface, by design.
+        array_filter($links, function(LinkInterface $item) { return true; });
+
+        $hashes = array_map('spl_object_hash', $links);
+        $this->links = array_combine($hashes, $links);
+    }
 }
--- src/Link.php
+++ src/Link.php
@@ -9,4 +9,19 @@ class Link implements EvolvableLinkInterface
 {
     use EvolvableLinkTrait;
 
+    /**
+     * Link constructor.
+     *
+     * @param string $rel
+     *   A single relationship to include on this link.
+     * @param string $href
+     *   An href for this link.
+     */
+    public function __construct($rel = '', $href = '')
+    {
+        if ($rel) {
+            $this->rel[$rel] = true;
+        }
+        $this->href = $href;
+    }
 }
